afa9a26bafb5852bd695c08103ae883e97056b73
lili <lelesano@espe.edu.ec>
2026-06-29 20:10:48 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/test/java/com/aerolinea/reservas/ReservasReactivasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aerolinea.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasReactivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
